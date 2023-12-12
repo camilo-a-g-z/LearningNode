fc82b7bcb38341cc0fa4bcb33ad60c3747a9fbcb
camilo-a-g-z <cagarciaz@udistrital.edu.co>
2023-12-12 17:54:30 -0500

    Se avanza en asinc await

---
+++ 4.async-await.js
@@ -0,0 +1,13 @@
+//const { promisify } = require("node:util");
+//const readFilePromise = promisify(fs.readFile); //solo usar cuando no tenga un metodo nativo de promesas
+const { readFile } = require("node:fs/promises");
+(async () => {
+  console.log("Leyendo el primer archivo...");
+  const text = await readFile("./archivo.txt", "utf-8");
+  console.log(text);
+  console.log("Hacer cosas mientras lee el archivo");
+
+  console.log("Leyendo el segundo archivo...");
+  const textSecond = await readFile("./archivo2.txt", "utf-8");
+  console.log(textSecond);
+})();
--- 4.async-await.mjs
+++ 4.async-await.mjs
@@ -1,20 +1,11 @@
-const fs = require("node:fs/promises");
-
 //const { promisify } = require("node:util");
 //const readFilePromise = promisify(fs.readFile); //solo usar cuando no tenga un metodo nativo de promesas
-
+import { readFile } from "node:fs/promises";
 console.log("Leyendo el primer archivo...");
-fs.readFile("./archivo.txt", "utf-8")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
+const text = await readFile("./archivo.txt", "utf-8");
+console.log(text);
 console.log("Hacer cosas mientras lee el archivo");
 
 console.log("Leyendo el segundo archivo...");
-const textSecond = fs.readFile("./archivo2.txt", "utf-8").then((data) => {
-  console.log(data);
-});
+const textSecond = await readFile("./archivo2.txt", "utf-8");
+console.log(textSecond);
