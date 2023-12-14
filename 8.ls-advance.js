17bbf994f2d13f97a843b3d340b4cd31a5fd1929
camilo-a-g-z <cagarciaz@udistrital.edu.co>
2023-12-14 15:52:30 -0500

    Se genera proyecto node

--- 8.ls-advance.js
+++ 8.ls-advance.js
@@ -1,26 +1,38 @@
 const fs = require("node:fs/promises");
+const path = require("node:path");
 
 const folder = process.argv[2] ?? "."; //el ?? es para que si no se le pasa un argumento, tome el valor de "."
 
 async function ls(directory) {
+  let files;
   try {
-    const files = await fs.readdir(directory);
+    files = await fs.readdir(directory);
   } catch {
     console.log("Error al leer el directorio");
     process.exit(1);
   }
+  const filesPromises = files.map(async (file) => {
+    const filePath = path.join(directory, file);
+    let stats;
+    try {
+      stats = await fs.stat(filePath); //stat es una funcion que devuelve un objeto con informacion del archivo
+    } catch {
+      console.log("Error al leer el archivo");
+      process.exit(1);
+    }
+    const isDirectory = stats.isDirectory();
+    const fileType = isDirectory ? "d" : "-";
+    const filesSize = stats.size;
+    const fileModified = stats.mtime.toLocaleString();
+    return `${fileType} ${file.padEnd(20)} ${filesSize
+      .toString()
+      .padStart(10)} ${fileModified} `;
+  });
+  const fileInfo = await Promise.all(filesPromises);
+
+  fileInfo.forEach((file) => {
+    console.log(file);
+  });
 }
 
-fs.readdir(folder, (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    files.forEach((element) => {
-      const filePath = path.join(folder, element);
-
-      fs.stat(filePath);
-      console.log(element);
-    });
-  }
-});
+ls(folder);
