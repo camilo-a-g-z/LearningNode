f1590c6fdfada25a9c634ee8a8c09f1978eb0e4f
camilo-a-g-z <cagarciaz@udistrital.edu.co>
2023-12-22 16:56:34 -0500

    Se profundiza en express

--- Clase2/pokemon/3.express.js
+++ Clase2/pokemon/3.express.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const ditto = require("./ditto.json");
 const app = express();
+app.disable("x-powered-by");
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+/*
+//FORMA LARGA
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hola mundo" });
-});
-
-app.post("/pokemon", (req, res) => {
+//tambien puede ser por direcciones
+// app.use("/pokemon/*", (req, res, next) => { -> /pokemon/1
+app.use((req, res, next) => {
+  if (req.method !== "POST") return next();
+  if (req.headers["content-type"] !== "application/json") return next();
+  //solo se ejecuta si es un post y si el content-type es json
   let body = "";
-
   //escuchar el evento data
   req.on("data", (chunk) => {
     body += chunk.toString();
@@ -18,10 +23,28 @@ app.post("/pokemon", (req, res) => {
     const data = JSON.parse(body);
     //llamara a una base de datos para guardar la info
     data.timestamp = Date.now();
-    res.status(201).json(data);
+    //mutar la request y meter la info en el req.body
+    req.body = data;
+    next();
   });
+});
+*/
+app.get("/", (req, res) => {
+  res.json({ message: "Hola mundo" });
+});
+
+app.get("/pokemon/ditto", (req, res) => {
+  res.json(ditto);
+});
+
+app.post("/pokemon", (req, res) => {
+  res.status(201).json(req.body);
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 Not Found</h1>");
 });
