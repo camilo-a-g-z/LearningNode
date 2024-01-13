f29a2cb16197cac64623bbd75b82d21ef51e0ff9
camilo-a-g-z <cagarciaz@udistrital.edu.co>
2024-01-13 17:56:29 -0500

    Se empieza con patron MVC

--- Clase4/routes/movies.js
+++ Clase4/routes/movies.js
@@ -3,67 +3,49 @@ import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 const movies = require("../movies.json");
 import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
+import { MovieModel } from "../models/movie.js";
 
 export const moviesRouter = Router();
 
-moviesRouter.get("/", (req, res) => {
+moviesRouter.get("/", async (req, res) => {
   const origin = req.header("origin");
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
     //con esto se evita el problema de cors que es cuando se intenta acceder a un recurso desde un dominio distinto
     res.header("Access-Control-Allow-Origin", origin); //para que solo se pueda acceder desde ese dominio
   }
   const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    );
-    return res.json(filteredMovies);
-  }
+  const movies = await MovieModel.getAll({ genre });
   res.json(movies);
 });
 
-moviesRouter.get("/:id", (req, res) => {
+moviesRouter.get("/:id", async (req, res) => {
   //path to regexp
   const { id } = req.params;
-  const movie = movies.find((movie) => movie.id === id);
-  if (movie) return res.json(movie);
+  const movie = await MovieModel.getById({ id });
   res.status(404).json({ message: "Movie not found" });
 });
 
-moviesRouter.post("/", (req, res) => {
+moviesRouter.post("/", async (req, res) => {
   const result = validateMovie(req.body);
   if (result.error)
     return res.status(400).json({ message: JSON.parse(result.error.message) });
-  const newMovie = {
-    id: randomUUID(),
-    ...result.data,
-  };
-  movies.push(newMovie);
+  const newMovie = await MovieModel.create(result.data);
   res.status(201).json(newMovie); //sirve para actualizar la cache del cliente
 });
 
-moviesRouter.delete("/:id", (req, res) => {
+moviesRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-  if (movieIndex < 0)
+  const result = await MovieModel.delete({ id });
+  if (movieIndex === false)
     return res.status(404).json({ message: "Movie not found" });
-  movies.splice(movieIndex, 1);
   res.sendStatus(204);
 });
 
-moviesRouter.patch("/:id", (req, res) => {
+moviesRouter.patch("/:id", async (req, res) => {
   const result = validatePartialMovie(req.body);
   if (!result.success)
     return res.status(400).json({ message: JSON.parse(result.error.message) });
   const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-  if (movieIndex < 0)
-    return res.status(404).json({ message: "Movie not found" });
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data,
-  };
-  movies[movieIndex] = updateMovie;
-  console.log(updateMovie);
+  const updateMovie = await MovieModel.update({ id, input: result.data });
   return res.json(updateMovie);
 });
